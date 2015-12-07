2709a0deab8d4b8efed10e40fe0a243578db4a1d
Kenneth Liu <ksliu25@gmail.com>
2015-12-07 12:49:59 -0600

    Fix store factory

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-# describe Store do
+describe Store do
 
-#   it 'can be created' do
-#     store = create :store
-#     expect(store).to_not be_nil
-#   end
+  it 'can be created' do
+    store = create :store
+    expect(store).to_not be_nil
+  end
 
-#   it 'needs tests to be written!' do
-#     pending('write tests for Store!')
-#   end
+  it 'needs tests to be written!' do
+    pending('write tests for Store!')
+  end
 
-# end
+end
