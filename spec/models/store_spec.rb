e8dc700d3c5cbcaa5cdf9ea4aa4a9cf3fefe0826
Kenneth Liu <ksliu25@gmail.com>
2015-12-07 19:15:45 -0600

    added before_validation callback spec

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -17,7 +17,18 @@ describe Store, type: :model do
   	it { should validate_presence_of(:longitude) }  	  
   	it { should validate_presence_of(:owner_id) }
 
-  	it "should generate a new daily if "
+  	it "should generate a new daily if nothing is given" do
+  		store = Store.new(name: "DBC Burgers", address: "351 W Hubbard St, Chicago, IL 60654, USA", latitude: 41.8897170, longitude: -87.6376110, owner_id: 1)
+  		store.valid?
+  		store.daily_code != nil
+  	end
+
+  	it "should generate a new daily code to whatever is given" do
+  		store = Store.new(name: "DBC Burgers", address: "351 W Hubbard St, Chicago, IL 60654, USA", latitude: 41.8897170, longitude: -87.6376110, daily_code: "DBCRocks!", owner_id: 1)
+  		store.valid?
+  		store.daily_code == "DBCRocks!"
+  	end
+
   	# it 'has an owner_id' do
   	# 	expect store.
   	# end
