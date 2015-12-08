9461cb9fc4b3167a9f1bbd7e02e39446d36826a7
Kenneth Liu <ksliu25@gmail.com>
2015-12-08 09:53:35 -0600

    Add visit model validations

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,4 +1,19 @@
 class Visit < ActiveRecord::Base
 	belongs_to :store
 	belongs_to :customer, class_name: "User"
+
+	validate :match_daily_code, on: :create
+
+	validates_presence_of :store_id, :customer_id, :near_location, :check_in_code
+
+	validates :near_location,
+		:inclusion => { :in => [true],
+		:message => "visit must be near store location!" }
+
+	def match_daily_code
+		@store = Store.find(self.store_id)
+		errors.add(:check_in_code, "Does not match daily code!") unless self.check_in_code == @store.daily_code
+	end
+
+
 end
