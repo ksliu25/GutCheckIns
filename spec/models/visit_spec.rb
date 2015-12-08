7f1c1ab2daa302ed5e5cd79cdf3c16ea465bebd6
Kenneth Liu <ksliu25@gmail.com>
2015-12-08 13:43:21 -0600

    fix inclusion validation of near_location

--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -16,7 +16,12 @@ describe Visit, type: :model do
 	  	it { should validate_presence_of(:store_id) }  
 	  	it { should validate_presence_of(:customer_id) }  
 	  	it { should validate_presence_of(:near_location) }  
-	  	it { should validate_presence_of(:check_in_code) }  	  
+	  	it { should validate_presence_of(:check_in_code) } 
+	  	it do 
+	  		should validate_inclusion_of(:near_location).
+	  		in_array([true]).
+	  		with_message("visit must be near store location!")
+	  	end 	  
 
 	end
 
