d47d199c65a5b7bfd0802ef540c8e7e48c36f426
Kenneth Liu <ksliu25@gmail.com>
2015-12-10 20:50:41 -0600

    refactor user api spec for let block

--- spec/apis/stores_api_spec.rb
+++ spec/apis/stores_api_spec.rb
@@ -8,9 +8,12 @@ describe StoresApi do
   include Rack::Test::Methods
 
   describe 'e.g. GET, POST, PUT, etc.' do
+  	before do
+  		@user = User.create(username: "test_user_4", password: "password")
+  	end
   	context "POST /stores" do
   		it "can create a store given a user" do
-  			@user = User.create(username: "test_user_4", password: "password")
+  			# @user = User.create(username: "test_user_4", password: "password")
   			params = {name: "DBC Burgers", address: "351 W Hubbard St, Chicago, IL 60654, USA", latitude: 41.8897170, longitude: -87.6376110, daily_code: "DBCRocks!", owner_id: "#{@user.id}" }
   			post '/stores', params
   			expect(last_response.status).to eq(201)
@@ -20,13 +23,16 @@ describe StoresApi do
 
   	context "GET /stores" do
   		it "returns all stores" do
-  			
-  		# 	User.create(username: "test_user_5", password: "password")
-  		# 	p User.all
-  		# 	params = {}
-  		# 	get '/stores'
-  		# 	response.body.should == ""
+	  		params = {name: "DBC Burgers", address: "351 W Hubbard St, Chicago, IL 60654, USA", latitude: 41.8897170, longitude: -87.6376110, daily_code: "DBCRocks!", owner_id: "#{@user.id}" }
+	  		post '/stores', params
+  			get '/stores'
+  			expect(last_response.status).to eq(200)
+  			expect(JSON.parse(last_response.body)).to eq("data" => [{"object_type"=>"store", "id"=>"2", "name"=>"DBC Burgers", "daily_code"=>"DBCRocks!", "owner_id"=>2}] )
   		end
+  	end
+
+  	context "GET /stores/:id" do
+  		
   	end
 
   # 	end
--- spec/apis/users_api_spec.rb
+++ spec/apis/users_api_spec.rb
@@ -12,38 +12,38 @@ describe UsersApi do
 
   	end
 
+  	let(:params_1){ {username: "test_user_1", password: "password"} }
+  	let(:params_2){ {username: "test_user_2", password: "password"} }
+
   	context 'POST /users' do
 			it 'can create a single user' do
-				params = { username: "Kenneth", password: "password" }
-				post '/users', params
+				post '/users', params_1
 				expect(last_response.status).to eq(201)
-				expect(JSON.parse(last_response.body)).to eq({"data" => { "object_type" => "user", "id" => "2", "username" => "Kenneth" }})
+				expect(JSON.parse(last_response.body)).to eq({"data" => { "object_type" => "user", "id" => "3", "username" => "test_user_1" }})
 			end
   	end
 
   	context 'GET /users' do
   		it 'returns an array of users' do
-  			params_1 = { username: "test_user_1", password: "password" }
-  			params_2 = { username: "test_user_2", password: "password" }
   			post '/users', params_1
   			post 'users', params_2
 	  		get '/users'
 	  		expect(last_response.status).to eq(200)
-	  		expect(JSON.parse(last_response.body)).to eq({"data" => [{ "object_type" => "user", "id" => "3", "username" => "test_user_1" }, { "object_type" => "user", "id" => "4", "username" => "test_user_2" }]})
+	  		expect(JSON.parse(last_response.body)).to eq({"data" => [{ "object_type" => "user", "id" => "4", "username" => "test_user_1" }, { "object_type" => "user", "id" => "5", "username" => "test_user_2" }]})
   		end
 
-  		context 'GET /users/:user_id' do
-  			it 'returns the userobject associated with the user_id' do
-  				params_1 = { username: "test_user_3", password: "password" }
-  				post '/users', params_1
-  				@user = User.find_by_username("test_user_3")
-  				get "/users/#{@user.id}"
-  				expect(last_response.status).to eq(200)
-  				expect(JSON.parse(last_response.body)).to eq({"data" => { "object_type" => "user", "id" => "#{@user.id}", "username" => "test_user_3" }})
-  			end
-
-  		end
   	end
+
+		context 'GET /users/:user_id' do
+			it 'returns the userobject associated with the user_id' do
+				post '/users', params_1
+				@user = User.find_by_username("test_user_1")
+				get "/users/#{@user.id}"
+				expect(last_response.status).to eq(200)
+				expect(JSON.parse(last_response.body)).to eq({"data" => { "object_type" => "user", "id" => "#{@user.id}", "username" => "test_user_1" }})
+			end
+
+		end
 
   end
 
