49fda84cd9837c99eef4e4e913bfdb8245939ec5
Kenneth Liu <ksliu25@gmail.com>
2016-04-23 11:46:39 -0500

    refactor generate_4_digit_string

--- app/models/store.rb
+++ app/models/store.rb
@@ -33,9 +33,7 @@ class Store < ActiveRecord::Base
 	private
 
 	def generate_4_digit_string
-		string = ""
-		4.times { string += ([0,1,2,3,4,5,6,7,8,9].sample).to_s }
-		return string
+		(0..3).map{rand(9)}.join("")
 	end
 
 
